d23200dcea2b45cf5bacba57b1ef8fad13400b9f
Alberto Sierra <alberto.sierra@murciaeduca.es>
2022-03-09 19:14:04 +0100

    Ejercicio 3: CRUD acogimientos

---
+++ app/Http/Resources/AcogimientoResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AcogimientoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'familia acogida' => new UserResource($this->user),
+            'acogido' => new AcogidoResource($this->acogido)
+        ];
+    }
+}
--- app/Models/Acogimiento.php
+++ app/Models/Acogimiento.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Acogimiento extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that owns the acogimiento.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the acogido that owns the acogimiento.
+     */
+    public function acogido()
+    {
+        return $this->belongsTo(Acogido::class);
+    }
 }
